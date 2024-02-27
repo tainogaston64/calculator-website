54f2931e51b7223b2a61d733e510f27ebdbdbcfe
tainogaston64 <161156085+tainogaston64@users.noreply.github.com>
2024-02-27 10:50:37 -0800

    Add files via upload

---
+++ 10j.js
@@ -0,0 +1,21 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+    displayCalculation();
+
+    function updateCalculation(value) {
+        calculation += value;
+
+        displayCalculation();
+
+        localStorage.setItem('calculation', calculation);
+    }
+
+    function saveCalculation() {
+        localStorage.setItem('calculation', calculation);
+
+    }
+    function displayCalculation() {
+        document.querySelector('.js-display-calculation')
+            .innerHTML = calculation;
+
+    }
